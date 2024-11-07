6eb58ea27e4abb172ff5be30ce3bcf283cf14524
Lucas Cosme <santoslucascosme12@gmail.com>
2024-11-07 19:55:33 -0300

    first commit

---
+++ Polimorfismo_CSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    { 
+        var cachorro = new Cachorro();
+        cachorro.EmitirSom();
+
+        var gato = new Gato();
+        gato.EmitirSom();
+    }
+    public class Animal
+    {
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine("Som do animal");
+        }
+    }
+    public class Cachorro : Animal
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Au au au au");
+        }
+    }
+
+    public class Gato : Animal
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Miau miau");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Polimorfismo_CSharp
